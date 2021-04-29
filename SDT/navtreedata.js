03f3a26467d943559513578faca2328eddd17b5c
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-04-29 12:39:01 +0200

    Squashed history of branch 'gh-pages'

---
+++ SDT/navtreedata.js
@@ -0,0 +1,34 @@
+var NAVTREE =
+[
+  [ "Sound Design Toolkit", "index.html", [
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_s_d_t__fileusage_8h_source.html",
+"group__breaking.html#gad80e128f84e8aab1b403a94e53216d0f",
+"group__delay.html#ga29fa258d35a81249072073b5783459e7",
+"group__json__friction.html#gad17edc6c5f4408ec23909a0445538a4c",
+"group__osc__macros.html#gae613f6eea99b51cb28ff1cdc303a8af9",
+"group__solids.html#gaffc95f5df4212748c12a6c55f8461433"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
